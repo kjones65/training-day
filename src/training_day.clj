f33a1adc410a212a78942f0794ff69ffbc52b089
KAJ <kaj@csdavidson.com>
2017-10-07 20:17:09 -0400

    All tests pass for training-day

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square
+  "calculates the square of x"
+  [x]
+  (* x x)
+  )
 
-(defn average [a b]
-  ":(")
+(defn average
+  "calculates the average of two numbers"
+  [a b]
+  (/ (+ a b) 2)
+  )
